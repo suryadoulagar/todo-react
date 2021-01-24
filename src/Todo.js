f7dca0ee255ef3140a263f1bda0d681899733183
suryadoulagar <suryadoulagar@gmail.com>
2021-01-24 21:03:27 +0530

    full CRUD

--- src/Todo.js
+++ src/Todo.js
@@ -18,26 +18,31 @@ const useStyles = makeStyles((theme) => ({
 
 
 function Todo(props) {
-    
+    const classes = useStyles();
     const [open, setOpen] = useState(false);
     const [input, setInput] = useState('');
 
     const handleOpen = () => {
         setOpen(true);
     };
-    const updateWish = () => {
+    const updateTodo = () => {
+        db.collection('todos').doc(props.todo.id).set({
+            todo: input
+        }, {merge: true})
         setOpen(false);
+        setInput("");
     }
 
     return (
         <>
         <Modal className={classes.paper}
-         open = {open} 
+         open = {open}
          onClose = { e => setOpen(false)}
          >
              <div  className={classes.paper}>
-                 <h1> edit wish</h1>
-                 <input value={input} onChange={event => setInput(event.target.value)} />
+                 <h1> edit your wish</h1>
+                 <input placeholder={props.todo.todo} value={input} onChange={event => setInput(event.target.value)} />
+                 <Button onClick= {updateTodo}>update</Button>
              </div>
          </Modal>
  
